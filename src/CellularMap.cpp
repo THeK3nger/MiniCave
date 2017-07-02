91f26b097c659b9b64472e26b8379113344a7339
Davide Aversa <thek3nger@gmail.com>
2017-07-02 11:18:47 +0200

    Inlined definitions and declarations

--- src/CellularMap.cpp
+++ src/CellularMap.cpp
@@ -35,20 +35,35 @@ public:
      * @param height                Desired map height.
      * @param walls_probability     Initial percentage of walls.
      */
-    CellularMap(int width, int height, int walls_probability);
-    ~CellularMap();
+    CellularMap(int width, int height, int walls_probability)  :
+            height(height), width(width), walls_probability(walls_probability)
+    {
+        map = new int[height*width];
+        randomFill();
+    }
+
+    ~CellularMap() { delete[] map; }
 
     /*!
      * Execute a step of the automata algorithm creating the cave or
      * Smoothing the existing ones.
      */
-    void evolveMap();
+    void evolveMap() { evolveMap(CellularMap::CM_CONSERVATIVE); }
 
     /*!
      * Execute a step of the automata algorithm creating the cave or
      * Smoothing the existing ones using a specific rule set.
      */
-    void evolveMap(RuleSet rule);
+    void evolveMap(RuleSet rule) {
+        // By initializing column in the outer loop, its only created ONCE
+        for (int row = 0; row <= height - 1; row++)
+        {
+            for (int column = 0; column <= width - 1; column++)
+            {
+                map[getIndex(column, row)] = placeWallLogic(column, row, rule);
+            }
+        }
+    }
 
     /*!
      * Get the element in position <x,y>.
@@ -79,14 +94,64 @@ private:
     /*
     * Initialize the map with a random amount of walls.
     */
-    void randomFill();
+    void randomFill() {
+        srand (time(NULL));
+        for (int row = 0; row < this->height; row++) {
+            for (int column = 0; column < this->width; column++)
+            {
+                // If coordinates lie on the the edge of the map (creates a border).
+                // We want border on a map.
+                if (column == 0 || row == 0 || (column == width - 1) || (row == height - 1))
+                {
+                    map[getIndex(column, row)] = 1;
+                }
+                    // Else, fill with a wall a random percent of the time
+                else
+                {
+                    int mapMiddle = (height / 2);
+
+                    // Add an empty line in the center of the map. This leads to better maps.
+                    // There is no other reason.
+                    if (row == mapMiddle)
+                    {
+                        map[getIndex(column, row)] = 0;
+                    }
+                    else
+                    {
+                        map[getIndex(column, row)] = (rand() % 101 >= walls_probability) ? 0 : 1;
+                    }
+                }
+            }
+        }
+    }
 
     /*
     * Execute the Cellular Automata rules on every tile of the map.
     *
     * @return The new value of tile <x,y>.
     */
-    int  placeWallLogic(int x, int y, RuleSet rule);
+    int  placeWallLogic(int x, int y, RuleSet rule) {
+        int numWalls = getAdjacentWalls(x, y, 1, 1);
+        int numWalls2 = getAdjacentWalls(x, y, 2, 2);
+
+        if (map[getIndex(x,y)] == 1)
+        {
+            return (numWalls >= 3) ? 1 : 0;
+        }
+        else
+        {
+            if (rule == CellularMap::CM_CONSERVATIVE) {
+                if (numWalls >= 5 || numWalls2 <= 2) {
+                    return 1;
+                }
+            } else {
+                if (numWalls >= 5) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
 
     /*
     * Get the sum of adjacent walls to a given tile <x,y>
@@ -97,7 +162,28 @@ private:
     * @param scope_y    The search scope radius in y direction.
     * @return           The number of walls in the scope of <x,y>.
     */
-    int  getAdjacentWalls(int x, int y, int scope_x, int scope_y);
+    int  getAdjacentWalls(int x, int y, int scope_x, int scope_y) {
+        int startX = x - scope_x;
+        int startY = y - scope_y;
+        int endX = x + scope_x;
+        int endY = y + scope_y;
+
+        int wallCounter = 0;
+
+        for (int iY = startY; iY <= endY; iY++) {
+            for (int iX = startX; iX <= endX; iX++)
+            {
+                if (!(iX == x && iY == y))
+                {
+                    if (isWall(iX,iY))
+                    {
+                        wallCounter += 1;
+                    }
+                }
+            }
+        }
+        return wallCounter;
+    }
 
     /*
     * Check if a tile is out of the map.
@@ -106,7 +192,7 @@ private:
     * @param y  The y coordinate in the map.
     * @return   True iff <x,y> is out of the map. False otherwise.
     */
-    bool isOutOfBound(int x, int y);
+    bool isOutOfBound(int x, int y) { return (x < 0 || y<0 || x>width - 1 || y > height - 1); }
 
     /*
     * Get the rom-major index of the tile <x,y>
@@ -124,129 +210,9 @@ private:
     * @param y  The y coordinate in the map.
     * @return   True iff <x,y> is a wall. False otherwise.
     */
-    bool isWall(int x, int y);
+    bool isWall(int x, int y) { return (isOutOfBound(x, y)) || (map[getIndex(x, y)] == 1); }
 
 };
-
-CellularMap::CellularMap(int width, int height, int walls_probability) :
-    height(height), width(width), walls_probability(walls_probability) 
-{
-    map = new int[height*width];
-    randomFill();
-}
-
-CellularMap::~CellularMap()
-{
-    delete map;
-}
-
-void CellularMap::randomFill()
-{
-    srand (time(NULL));
-    for (int row = 0; row < this->height; row++) {
-        for (int column = 0; column < this->width; column++)
-        {
-            // If coordinates lie on the the edge of the map (creates a border).
-            // We want border on a map.
-            if (column == 0 || row == 0 || (column == width - 1) || (row == height - 1))
-            {
-                map[getIndex(column, row)] = 1;
-            }
-            // Else, fill with a wall a random percent of the time
-            else
-            {
-                int mapMiddle = (height / 2);
-
-                // Add an empty line in the center of the map. This leads to better maps.
-                // There is no other reason.
-                if (row == mapMiddle)
-                {
-                    map[getIndex(column, row)] = 0;
-                }
-                else
-                {
-                    map[getIndex(column, row)] = (rand() % 101 >= walls_probability) ? 0 : 1;
-                }
-            }
-        }
-    }
-}
-
-void CellularMap::evolveMap()
-{
-    evolveMap(CellularMap::CM_CONSERVATIVE);
-}
-
-
-void CellularMap::evolveMap(RuleSet rule) {
-    // By initializing column in the outer loop, its only created ONCE
-    for (int row = 0; row <= height - 1; row++)
-    {
-        for (int column = 0; column <= width - 1; column++)
-        {
-            map[getIndex(column, row)] = placeWallLogic(column, row, rule);
-        }
-    }
-}
-
-int CellularMap::placeWallLogic(int x, int y,RuleSet rule)
-{
-    int numWalls = getAdjacentWalls(x, y, 1, 1);
-    int numWalls2 = getAdjacentWalls(x, y, 2, 2);
-
-    if (map[getIndex(x,y)] == 1)
-    {
-        return (numWalls >= 3) ? 1 : 0;
-    }
-    else
-    {
-        if (rule == CellularMap::CM_CONSERVATIVE) {
-            if (numWalls >= 5 || numWalls2 <= 2) {
-                return 1;
-            }
-        } else {
-            if (numWalls >= 5) {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
-int CellularMap::getAdjacentWalls(int x, int y, int scope_x, int scope_y)
-{
-    int startX = x - scope_x;
-    int startY = y - scope_y;
-    int endX = x + scope_x;
-    int endY = y + scope_y;
-
-    int wallCounter = 0;
-
-    for (int iY = startY; iY <= endY; iY++) {
-        for (int iX = startX; iX <= endX; iX++)
-        {
-            if (!(iX == x && iY == y))
-            {
-                if (isWall(iX,iY))
-                {
-                    wallCounter += 1;
-                }
-            }
-        }
-    }
-    return wallCounter;
-}
-
-bool CellularMap::isOutOfBound(int x, int y)
-{
-    return (x < 0 || y<0 || x>width - 1 || y > height - 1);
-}
-
-bool CellularMap::isWall(int x, int y)
-{
-    // Out of bound is considered a wall.
-    return (isOutOfBound(x, y)) || (map[getIndex(x, y)] == 1);
-}
 
 using namespace std;
 
@@ -265,15 +231,8 @@ string mapToString(CellularMap& cm) {
 int main() {
     // This is an use example for the CellularMap class.
     CellularMap cm(50, 50, 40);
-    for (int i=0;i<4;++i){
-        cm.evolveMap(CellularMap::CM_CONSERVATIVE);
-    }
-    for (int i=0;i<2;++i){
-        cm.evolveMap(CellularMap::CM_SMOOTH);
-    }
-    string mapS = mapToString(cm);
-    cout << mapS;
-    cout << "........." << endl;
-    getchar();
+    for (int i=0;i<4;++i){ cm.evolveMap(CellularMap::CM_CONSERVATIVE); }
+    for (int i=0;i<2;++i){ cm.evolveMap(CellularMap::CM_SMOOTH); }
+    cout << mapToString(cm) << endl;
     return 0;
 }
